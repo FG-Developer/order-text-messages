d3092ed2c090b05711284495c894840cd9e763e2
ilyas demirtas <ilyasdemirtas@ilyass-MacBook-Pro.local>
2019-03-28 00:57:44 +0300

    Added message schema migration.

---
+++ database/migrations/2019_03_27_181819_create_messages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('restaurant_name', 100);
+            $table->tinyInteger('delivery_time');
+            $table->string('customer_phone_number', 50);
+            $table->integer('status')->index();
+            $table->string('error_text', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
