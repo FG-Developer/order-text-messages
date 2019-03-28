756b4f2f8aaac1fb979b6e508c587fe8e298ce66
İlyas Demirtaş <ilyas.demirtas@markakod.com>
2019-03-28 18:35:57 +0300

    Defined fillable fields for crud.
    Signed-off-by: İlyas Demirtaş <ilyas.demirtas@markakod.com>

--- app/Message.php
+++ app/Message.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    //
+    protected $fillable = [
+        'restaurant_name',
+        'delivery_time',
+        'customer_phone_number',
+        'status',
+        'error_text'
+    ];
 }
