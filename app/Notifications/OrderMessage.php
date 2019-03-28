afba615db6b10171a8cd4364882be1a0fac4700c
İlyas Demirtaş <ilyas.demirtas@markakod.com>
2019-03-28 18:33:21 +0300

    Added notification listener.
    Signed-off-by: İlyas Demirtaş <ilyas.demirtas@markakod.com>

---
+++ app/Notifications/OrderMessage.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Events\NotificationSent;
+use Illuminate\Notifications\Messages\NexmoMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class OrderMessage extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    private $message;
+    
+    /**
+     * Create a new notification instance.
+     *
+     * @param   string  $message
+     * @return void
+     */
+    public function __construct($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['nexmo'];
+    }
+    
+    /**
+     * Get the Nexmo / SMS representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return NexmoMessage
+     */
+    public function toNexmo($notifiable)
+    {
+        return (new NexmoMessage())
+            ->content($this->message);
+    }
+    
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->line('The introduction to the notification.')
+            ->action('Notification Action', url('/'))
+            ->line('Thank you for using our application!');
+    }
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
