d6b1a9d2ec928e9f88181a1fe9cc62333b5de82a
İlyas Demirtaş <ilyas.demirtas@markakod.com>
2019-03-28 18:23:33 +0300

    Integrated Nexmo Notification on Order store.
    Signed-off-by: İlyas Demirtaş <ilyas.demirtas@markakod.com>

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Http\Requests\API\OrderRequests;
+use App\Message;
+use App\Notifications\OrderMessage;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Notification;
 
 class OrderController extends Controller
 {
@@ -20,12 +24,38 @@ class OrderController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Http\Requests\API\OrderRequests  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(OrderRequests $request)
     {
-        //
+        $response = ['status' => 200, 'msg' => 'Success'];
+        $request_data = $request->all();
+        
+        $sms_text = "Thank You thank You for your order! We took your order as " . $request_data['restaurant_name']
+            . " and we will be delivered your order to you in "
+            . $request_data['delivery_time'] . " minutes.";
+        
+        try{
+            Notification::route('nexmo', $request_data['customer_phone_number'])->notify(new OrderMessage($sms_text));
+    
+            $when = now()->addMinutes($request_data['delivery_time'] + 90);
+            $sms_text = $request_data['restaurant_name'] . ". Enjoy your meal! We are happy for delivered your order. If you give vote, to our restaurant, we will be happy :)";
+            Notification::route('nexmo', $request_data['customer_phone_number'])->notify((new OrderMessage($sms_text))->delay($when));
+            
+            $request_data = array_merge($request_data, ['status' => 0]);
+        } catch (\Exception $e){
+            $request_data = array_merge($request_data, ['status' => $e->getCode(), 'error_text' => $e->getMessage()]);
+            $response = ['status' => $e->getCode(), 'msg' => $e->getMessage()];
+        }
+        
+        $insert = Message::create($request_data);
+    
+        if(!$insert){
+            $response = ['status' => 500, 'msg' => 'Internal Server Error'];
+        }
+        
+        return response()->json($response);
     }
 
     /**
