313dd839fe9c367cc9b545bb966643405f276d9e
ilyas demirtas <ilyasdemirtas@ilyass-MacBook-Pro.local>
2019-04-01 23:59:13 +0300

    Added API class actions for the list of messages.

--- app/Http/Controllers/API/MessageController.php
+++ app/Http/Controllers/API/MessageController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Http\Resources\Message as MessageResource;
+use App\Message;
+
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -14,7 +17,40 @@ class MessageController extends Controller
      */
     public function index()
     {
-        //
+        $messages = Message::where('status', '<>', 0)
+        ->where('created_at', '>=', now()->subDay())
+        ->orderBy('id', 'DESC')
+        ->paginate();
+
+        return MessageResource::collection($messages);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function lastSentMessages(Request $request)
+    {
+        $messages = Message::where('status', 0)
+        ->orderBy('id', 'DESC')
+        ->limit(50);
+
+        $keyword = $request->get('q', null);
+        if($keyword){
+
+            if(strlen($keyword) < 3){
+                return response()->json(['The search keyword length must be at least 3 char.'], 422);
+            }
+
+            $messages = $messages->where('phone_number', 'LIKE', '%' . $keyword . '%')
+            ->orWhere('message', 'LIKE', '%' . $keyword . '%')
+            ->orWhere('restaurant_name', 'LIKE', '%' . $keyword . '%');
+        }
+
+        $messages = $messages->get();
+
+        return MessageResource::collection($messages);
     }
 
     /**
