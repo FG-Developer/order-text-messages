1dfd374e64685289059356213c09c1f8db987c06
ilyas demirtas <ilyasdemirtas@ilyass-MacBook-Pro.local>
2019-04-01 23:37:43 +0300

    Bugs fixed on API Controller for order receive and messages send.

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Requests\API\OrderRequests;
 use App\Message;
+
 use App\Notifications\OrderMessage;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -31,31 +32,36 @@ class OrderController extends Controller
     {
         $response = ['status' => 200, 'msg' => 'Success'];
         $request_data = $request->all();
-        
-        $sms_text = "Thank You thank You for your order! We took your order as " . $request_data['restaurant_name']
-            . " and we will be delivered your order to you in "
+
+        $sms_text = "Thank You for your order! We took your order as " . $request_data['restaurant_name']
+            . " and We will have your order delivered within "
             . $request_data['delivery_time'] . " minutes.";
-        
-        try{
-            Notification::route('nexmo', $request_data['customer_phone_number'])->notify(new OrderMessage($sms_text));
-    
-            $when = now()->addMinutes($request_data['delivery_time'] + 90);
-            $sms_text = $request_data['restaurant_name'] . ". Enjoy your meal! We are happy for delivered your order. If you give vote, to our restaurant, we will be happy :)";
-            Notification::route('nexmo', $request_data['customer_phone_number'])->notify((new OrderMessage($sms_text))->delay($when));
-            
-            $request_data = array_merge($request_data, ['status' => 0]);
-        } catch (\Exception $e){
-            $request_data = array_merge($request_data, ['status' => $e->getCode(), 'error_text' => $e->getMessage()]);
-            $response = ['status' => $e->getCode(), 'msg' => $e->getMessage()];
-        }
-        
-        $insert = Message::create($request_data);
-    
-        if(!$insert){
+        $model_data = array_merge($request_data, ['status' => 1, 'status_msg' => 'In Queue', 'message' => $sms_text]);
+        $message = Message::create($model_data);
+
+        if($message){
+
+            $message->notify(new OrderMessage($sms_text));
+
+            $sms_text = $request_data['restaurant_name'] .
+            ". Enjoy your meal! We are happy for delivered your order. If you give a vote to our restaurant, we will be happy :)";
+            $model_data = array_merge($request_data, ['status' => 1, 'status_msg' => 'In Queue', 'message' => $sms_text]);
+            $message = Message::create($model_data);
+
+            if($message){
+                $when = now()->addMinutes($request_data['delivery_time'] + 90);
+                $message->notify((new OrderMessage($sms_text))->delay($when));
+            } else {
+                $response = ['status' => 500, 'msg' => 'Internal Server Error'];
+                return response()->json($response, 500);
+            }
+
+        } else {
             $response = ['status' => 500, 'msg' => 'Internal Server Error'];
+            return response()->json($response, 500);
         }
-        
-        return response()->json($response);
+
+        return response()->json($response, 201);
     }
 
     /**
